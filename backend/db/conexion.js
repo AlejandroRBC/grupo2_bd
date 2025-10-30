52daf5140e282b7b34531a16881ba3be2b240f39
mhlidia <224844611+mhlidia@users.noreply.github.com>
2025-10-30 08:25:25 -0400

    cambio en conexion

--- backend/db/conexion.js
+++ backend/db/conexion.js
@@ -1,17 +1,10 @@
 import pg from "pg";
 
-/*export const pool = new pg.Pool({
+export const pool = new pg.Pool({
   user: "postgres",
   host: "localhost",
   database: "bdhotel",
   password: "admin",
   port: 5432,
-});*/
-
-export const pool = new pg.Pool({
-  user: "admin",
-  host: "localhost",
-  database: "hotel_db",
-  password: "admin123",
-  port: 5432,
 });
+
